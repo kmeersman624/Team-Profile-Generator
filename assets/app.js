fa5743d1b833361c9b01007ad93ac38aba4feaa6
kmeersman <katie4682@yahoo.com>
2020-05-31 15:52:42 -0600

    Added inquirer to app.js and constructor to employee.js

--- assets/app.js
+++ assets/app.js
@@ -10,20 +10,84 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const teamMembers = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-
+function promptUser (employee) {
+  return inquirer
+    .prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "What is the team member's name?"
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "What is the team member's ID number?"
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "What is the team member's email?"
+        },
+        {
+            name: "role",
+            type: "list",
+            message: "What is the team member's role?",
+            choices: ["Manager", "Engineer", "Intern"],
+        },
+    ]).then(function(result) {
+        if (result.role === "Manager") {
+            inquirer.prompt([
+                {
+                    name: "phone",
+                    type: "input",
+                    message: "What is the manager's office phone number?"
+                }
+            ]).then(function (manager) {
+                const newManager = new Manager(employee.name, employee.id, employee.email, manager.phone);
+                teamMembers.push(newManager);
+            });
+        } else if (result.role === "Engineer") {
+            inquirer.prompt([
+                {
+                    name: "github",
+                    type: "input",
+                    message: "What is the team member's GitHub username?"
+                }
+            ]).then(function(engineer) {
+                const newEngineer = new Engineer(employee.name, employee.id, employee.email,engineer.github);
+                teamMembers.push(newEngineer);
+            });
+        } else if (result.role === "Intern") {
+            inquirer.prompt([
+                {
+                    name: "school",
+                    type: "input",
+                    message: "What school did the team member attend?"
+                }
+            ]).then(function(intern) {
+                const newIntern = new Intern(employee.name, employee.id, employee.email,intern.github);
+                teamMembers.push(newIntern);
+            });
+        }
+        .catch(function (err) {
+            console.log(err);
+    }
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-
+render(teamMembers);
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-
+function writeFile () {
+    return fs.writeFileSync(outputPath);
+}
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
